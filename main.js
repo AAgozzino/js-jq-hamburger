ec329b38514994eb1245ad8c40df4050676fb67a
Anna Agozzino <anna.agozzino91@gmail.com>
2020-09-01 16:36:05 +0200

    Add + remove class "active"

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 // al click sull'icona fas fa-bars
 // ->hamburger-menu + class="active"
-$('a .fas .fa-bars').click(
+$('a .fas.fa-bars').click(
   function () {
     $('.hamburger-menu').addClass('active')
+  }
+);
+
+// al click sull'icona fas fa-times
+//  -> hamburger-menu - class="active"
+$('a .fas.fa-times').click(
+  function () {
+    $('.hamburger-menu').removeClass('active')
   }
 );
